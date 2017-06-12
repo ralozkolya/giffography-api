9fb4ee2fa21ca003c143abb4cf17d71f8bddd439
Kolya <raloz1@gmail.com>
2017-06-12 15:57:19 +0400

    Create models with their migrations and controllers

---
+++ database/migrations/2017_06_12_114605_create_videos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('event')->nullable()->references('id')->on('events');
+            $table->integer('file')->nullable()->references('id')->on('files');
+            $table->integer('thumb')->nullable()->references('id')->on('files');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videos');
+    }
+}
