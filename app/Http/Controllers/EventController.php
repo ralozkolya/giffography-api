fd2f7dbe5fa99c599cecda1030add317c9b5c6b2
Kolya <raloz1@gmail.com>
2017-06-12 16:21:54 +0400

    Add event resource handler

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -14,17 +14,7 @@ class EventController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return Event::paginate(10);
     }
 
     /**
@@ -35,7 +25,7 @@ class EventController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //return 'wat';
     }
 
     /**
