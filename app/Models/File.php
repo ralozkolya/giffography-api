e9c2a777d43a35d37628aeaa52fdf139690b18d1
Kolya <raloz1@gmail.com>
2017-06-13 13:56:25 +0400

    Add field definitions

--- app/Models/File.php
+++ app/Models/File.php
@@ -5,6 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
 
+/**
+ * @property string name
+ * @property false|string path
+ * @property int size
+ * @property null|string mimetype
+ * @property null|string resolution
+ */
 class File extends Model
 {
     protected static function boot()
