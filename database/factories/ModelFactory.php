e7b8fabc8865e061ddf34a24163e0be28b04d943
Kolya <raloz1@gmail.com>
2017-07-13 00:25:02 +0400

    Change name in Event factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -25,7 +25,8 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
 
 $factory->define(App\Models\Event::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->words(2, true),
+        'en_name' => $faker->words(2, true),
+        'ka_name' => $faker->words(2, true),
         'date' => Carbon\Carbon::now()
     ];
 });
